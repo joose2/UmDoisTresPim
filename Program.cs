b578e59f4685f29d23d22a8fca3fb8498f8ee4a8
jose <jose.silva2000@etec.sp.gov.br>
2022-11-21 20:44:10 -0300

    atividade

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("QUE COMECE A CONTAGEM KKK");
+for (int n = 1; n <= 40; n++)
+    Console.Write(n % 4 == 0 ? "proximo\n" : $"{n,2} ");
